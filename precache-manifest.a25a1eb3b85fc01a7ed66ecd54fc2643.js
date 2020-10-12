00e7bb5e1311a068e3e6d86a26fcf59bc5df4917
yashik1991 <yashiksingla1991@gmail.com>
2020-10-12 13:30:39 +0530

    Updates

---
+++ precache-manifest.a25a1eb3b85fc01a7ed66ecd54fc2643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdeab1663a1cb52df9928bca395eed7c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "475b939cfb19c4320afd",
+    "url": "/monsters-rolodex/static/css/main.5ce13238.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475b939cfb19c4320afd",
+    "url": "/monsters-rolodex/static/js/main.d1877873.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
